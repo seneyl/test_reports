6ec71f94855c775d8a2f4c52e505919b043f8fac
seneyl <lauren@indexdata.com>
2025-04-22 17:00:54 -0600

    Create claimed_returned.sql

---
+++ claimed_returned.sql
@@ -0,0 +1,23 @@
+SELECT
+	(i.jsonb -> 'status' ->> 'date')::DATE as claimed_date,
+	i.jsonb ->> 'barcode' as item_barcode,
+	lt2."name" as library,
+	lt."name"  as location,
+	holdings."call_number",
+	i.jsonb ->> 'copyNumber' AS copy,
+	i.jsonb ->> 'volume' AS vol,
+	l.jsonb ->> 'actionComment' as claim_note,
+	l.jsonb ->> 'id' AS loan_id,
+	U.barcode as patron_barcode,
+	l.jsonb ->> 'userId' AS user_id
+FROM folio_circulation.loan l
+LEFT JOIN folio_inventory.item i on i.id = (l.jsonb ->> 'itemId')::uuid
+LEFT JOIN folio_users.users__t as u on u.id = (l.jsonb ->> 'userId')::uuid
+LEFT JOIN folio_inventory.location__t lt  on lt.id = (i.jsonb ->> 'effectiveLocationId')::uuid
+LEFT JOIN folio_inventory.loclibrary__t lt2 on lt2.id = lt.library_id
+LEFT JOIN folio_inventory.holdings_record__t as holdings on holdings.id = i.holdingsrecordid
+where i.jsonb -> 'status' ->> 'name' = 'Claimed returned'
+	AND l.jsonb ->> 'itemStatus' = 'Claimed returned'
+	AND l.jsonb ->> 'action' = 'claimedReturned'
+	AND lt.”name” ~* item_location,
+ORDER BY claimed_date asc;
