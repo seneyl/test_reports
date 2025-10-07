e9c2aa27fd466753a9ee824d00868652ce550bda
seneyl <lauren@indexdata.com>
2025-10-07 09:54:59 -0600

    Create loan_count_test.sql

---
+++ sample/loan_count_test.sql
@@ -0,0 +1,18 @@
+--metadb:function loan_count
+CREATE OR REPLACE FUNCTION loan_count (
+	start_date date DEFAULT '2000-01-01',
+	end_date date DEFAULT '2050-01-01')
+RETURNS TABLE(
+	item_id uuid,
+	loan_count bigint) AS
+$$
+SELECT item_id,
+	   count(*) AS loan_count
+	 FROM folio_circulation.loan__t
+	 WHERE loan_date::DATE >= 'start_date'
+         AND loan_date::DATE < 'end_date'
+	 GROUP BY item_id
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
