d4d31cd5c3e67a3bcb28d0c512941fd4eddbb472
seneyl <lauren@indexdata.com>
2026-02-09 08:27:03 -0700

    Update statements.sql

--- holdings/statements.sql
+++ holdings/statements.sql
@@ -5,19 +5,17 @@ DROP FUNCTION IF EXISTS holdings_statements;
 CREATE FUNCTION holdings_statements()
 RETURNS TABLE(
   instance_hrid text
-  holdings_hrid text
   title text
   holdings_statement text
   )
 AS $$
 select 
 hs.instance_hrid, 
-hs.holdings_hrid,
 it.title,
 string_agg(hs.public_note, '; ') as holdings_statement
 from folio_derived.holdings_statements hs
 left join folio_inventory.instance__t it on hs.instance_id = it.id
-group by hs.instance_hrid, hs.holdings_hrid, it.title
+group by hs.instance_hrid, it.title
 $$
 LANGUAGE SQL
 STABLE
