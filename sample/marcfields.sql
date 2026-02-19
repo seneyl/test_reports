6c577c6fceb7090f6aa8be76121e2b359d15a0bc
seneyl <lauren@indexdata.com>
2026-02-19 08:08:57 -0700

    Update marcfields.sql

--- sample/marcfields.sql
+++ sample/marcfields.sql
@@ -6,16 +6,18 @@ CREATE FUNCTION marc_fields()
 RETURNS TABLE(
 hrid text,
 field varchar,
-content varchar
+content varchar,
+field2 text
 )
 AS $$
 select
 it.hrid,
 mt.field,
 mt.content,
-erl.description
+mt2.field as field2
 from folio_source_record.marc__t mt
 left join folio_inventory.instance__t it on mt.instance_id = it.id 
+left join folio_source_record.marc__t mt2 on mt.instance_id = mt2.instance_id
 $$
 LANGUAGE SQL
 STABLE
