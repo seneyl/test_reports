94453e5f2dbfce3473ae367bcb41b6134471c328
seneyl <lauren@indexdata.com>
2026-09-18 11:38:33 -0600

    Create circulation_by_title1.sql

---
+++ circulation/circualtion3/circulation_by_title1.sql
@@ -0,0 +1,24 @@
+--metadb:function circulation_by_title1
+
+DROP FUNCTION IF EXISTS circulation_by_title1;
+
+CREATE FUNCTION circulation_by_title(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01')
+RETURNS TABLE(
+    title text,
+    loan_date timestamptz
+)
+AS $$
+select 
+it.title, 
+li.loan_date
+from folio_derived.loans_items li
+left join folio_inventory.holdings_record__t hrt on li.holdings_record_id = hrt.id
+left join folio_inventory.instance__t it on hrt.instance_id::uuid = it.id
+where (start_date <= loan_date::date AND loan_date::date < end_date)
+order by li.loan_date
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
